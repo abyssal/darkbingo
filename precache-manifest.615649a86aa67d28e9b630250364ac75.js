05dc000de4306356ee447981d4cb1b49c02cb1eb
abyssal <abyssal512@gmail.com>
2019-11-25 11:58:32 +1300

    Updates

---
+++ precache-manifest.615649a86aa67d28e9b630250364ac75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a2cfdf0f81a5b359656da798fe7eac",
+    "url": "/darkbingo/index.html"
+  },
+  {
+    "revision": "8891edc0b3b6a67b8e97",
+    "url": "/darkbingo/static/css/main.0b5e0d17.chunk.css"
+  },
+  {
+    "revision": "754ad09fcd319fcb8fc7",
+    "url": "/darkbingo/static/js/2.3111646c.chunk.js"
+  },
+  {
+    "revision": "8891edc0b3b6a67b8e97",
+    "url": "/darkbingo/static/js/main.54805be3.chunk.js"
+  },
+  {
+    "revision": "c581613668a75a705fad",
+    "url": "/darkbingo/static/js/runtime-main.d14895b9.js"
+  }
+]);

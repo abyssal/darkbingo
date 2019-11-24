fd8e7b5c0eb10fb97acd66e41eaaaad872299d2b
abyssal <abyssal512@gmail.com>
2019-11-25 12:34:55 +1300

    Added credits

--- src/App.js
+++ src/App.js
@@ -72,6 +72,7 @@ export default class AppComponent extends React.Component {
             </tbody>
           </table>
           <h1 className="header"><img alt="" src="lick.png" id="left" /> <a rel="noopener noreferrer" className="nostyle" target="_blank" href="http://twitch.tv/DarkViperAU">twitch.tv/DarkViperAU</a> <img alt="" src="lick.png" /></h1>
+          <p id='credits'>Made by <a href="http://twitter.com/abyssalnz">Abyssal</a></p>
         </div>
       </div>
     );
